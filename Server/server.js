35d012437d4fb6c0e864b345c9cce609e6075ece
frankmagoba <franklinmagoba4@gmail.com>
2020-06-06 12:09:11 +0300

    signup frontend

--- Server/server.js
+++ Server/server.js
@@ -1,54 +1,26 @@
+﻿require('rootpath')();
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const router = express.Router();
 const app = express();
-const config = require("config");
-const mongoose = require("mongoose");
-const usersRoute = require("./routes/user.route");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 
-/**
- * Configure the middleware.
- * bodyParser.json() returns a function that is passed as a param to app.use() as middleware
- * With the help of this method, we can now send JSON to our express application.
- */
-// app.use( bodyParser.urlencoded( { extended: false } ) );
-// app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-// We export the router so that the server.js file can pick it up
-// module.exports = router;
+// use JWT auth to secure the api
+app.use(jwt());
 
-// Combine react and node js servers while deploying( YOU MIGHT HAVE ALREADY DONE THIS BEFORE
-// What you need to do is make the build directory on the heroku, which will contain the index.html of your react app and then point the HTTP request to the client/build directory
+// api routes
+app.use('/users', require('./users/users.controller'));
 
-// if ( process.env.NODE_ENV === 'production' ) {
-// 	// Set a static folder
-// 	app.use( express.static( 'client/build' ) );
-// 	app.get( '*', ( req, res ) => res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) ) );
+// global error handler
+app.use(errorHandler);
 
-// }
-
-// Set up a port
-// const port = process.env.PORT || 5000;
-
-// app.listen( port, () => console.log( `Server running on port: ${port}` ) );
-
-//use config module to get the privatekey, if no private key set, end the application
-if (!config.get("myprivatekey")) {
-	console.error("FATAL ERROR: myprivatekey is not defined.");
-	process.exit(1);
-}
-
-//connect to mongodb
-mongoose
-	.connect("mongodb://localhost/nodejsauth", { useNewUrlParser: true })
-	.then(() => console.log("Connected to MongoDB..."))
-	.catch(err => console.error("Could not connect to MongoDB..."));
-
-
-app.use(express.json());
-//use users route for api/users
-app.use("/api/users", usersRoute);
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,37 +1,109 @@
 import React, { Component } from "react";
+import Toast from 'light-toast';
 
 export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: ' ',
+            lastName: ' ',
+            username: ' ',
+            password: ' '
+        };
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const form = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            username: this.state.username,
+            password: this.state.password
+        }
+        console.log(form);
+        Toast.info('Registering...', 3000, () => {
+            // do something after the toast disappears
+        });
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(form)
+        };
+        fetch('http://localhost:4000/users/register', requestOptions)
+            .then(response => {
+            response.status === 200 ? Toast.info('Successfully registered...', 3000, () => {
+                window.location = "/sign-in"
+            }) : Toast.info("error");
+            })
+            .then(data => console.log(data));
+        // window.location = "/retrieve"
+    }
+
     render() {
         return (
             <form>
-                <h3>Sign Up</h3>
+                <h3> New User</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input
+                        type="text"
+                        name='firstName'
+                        className="form-control"
+                        value={this.state.firstName}
+                        placeholder="First name"
+                        onChange={e => this.handleChange(e)}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" name='lastName'
+                        className="form-control" placeholder="Last name" value={this.state.lastName} onChange={e => this.handleChange(e)}
+                    />
                 </div>
 
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>User name</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        name='username'
+                        placeholder="Enter email"
+                        value={this.state.username}
+                        onChange={e => this.handleChange(e)}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input
+                        type="password"
+                        name='password'
+                        className="form-control"
+                        placeholder="Enter password"
+                        value={this.state.password}
+                        onChange={e => this.handleChange(e)}
+                    />
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button
+                    type="submit"
+                    className="btn btn-primary btn-block"
+                    onClick={(e) => this.onSubmit(e)}
+                >
+                    Sign Up
+        </button>
                 <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
+                    Already registered <a href="/sign-in">sign in?</a>
                 </p>
             </form>
-
         );
     }
 }
